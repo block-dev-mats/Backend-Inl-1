b637c450a2d7ec14a95b89bdf685abb3fc27d349
block-dev-mats <229818883+block-dev-mats@users.noreply.github.com>
2026-08-18 18:24:51 +0200

    add string test

--- add.test.mjs
+++ add.test.mjs
@@ -4,3 +4,7 @@ import { add } from "./add.js";
 test("adds 1 and 2", () => {
   expect(add(1, 2)).toBe(3);
 });
+
+test("throws when inputs are not numbers", () => {
+  expect(() => add("abc", "åäö")).toThrow();
+});
