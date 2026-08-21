2a7ac8e8d8e7a1cb38f56ead6bdfe02776e5e71e
block-dev-mats <229818883+block-dev-mats@users.noreply.github.com>
2026-08-21 11:50:11 +0200

    add block mining

--- block.mjs
+++ block.mjs
@@ -15,8 +15,18 @@ export class Block {
       this.index +
       this.previousHash +
       this.timestamp +
-      JSON.stringify(this.transactions);
+      JSON.stringify(this.transactions) +
+      this.nonce;
 
     return crypto.createHash("sha256").update(payload).digest("hex");
+  }
+
+  mineBlock(difficulty) {
+    const targetPrefix = "0".repeat(difficulty);
+
+    while (!this.hash.startsWith(targetPrefix)) {
+      this.nonce += 1;
+      this.hash = this.calculateHash();
+    }
   }
 }
