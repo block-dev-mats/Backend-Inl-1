59cf30ca71b713b187c1a796c0ceba3af790afb1
block-dev-mats <229818883+block-dev-mats@users.noreply.github.com>
2026-08-21 11:39:04 +0200

    add block hash

--- block.mjs
+++ block.mjs
@@ -1,9 +1,22 @@
+import crypto from "node:crypto";
+
 export class Block {
-  constructor(index, timestamp, transactions, previousHash) {
+  constructor(index, timestamp, transactions, previousHash = "") {
     this.index = index;
     this.timestamp = timestamp;
     this.transactions = transactions;
     this.previousHash = previousHash;
     this.nonce = 0;
+    this.hash = this.calculateHash();
+  }
+
+  calculateHash() {
+    const payload =
+      this.index +
+      this.previousHash +
+      this.timestamp +
+      JSON.stringify(this.transactions);
+
+    return crypto.createHash("sha256").update(payload).digest("hex");
   }
 }
